bdd5a1f9f06de4991153e3808b5f8bed30c2ab52
SachidanandSharma2162 <sharmasachidanand1111@gmail.com>
2026-07-10 19:22:26 +0530

    Added query parameters

--- DAY3-FPS/main.py
+++ DAY3-FPS/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path,HTTPException
+from fastapi import FastAPI, Path,HTTPException,Query
 import json
 
 
@@ -22,9 +22,32 @@ def get_patients():
     return data
 
 @app.get("/patients/{p_id}")
-def get_patient_by_id(p_id:str=Path(...,description="ID of the Patient in the DB",example="P001")):
+def get_patient_by_id(p_id:str=Path(...,description="ID of the Patient in the DB.",example="P001")):
     patients_data=load_data()
 
     if p_id in patients_data:
         return patients_data[p_id]
     raise HTTPException(status_code=404,detail="Patient not found!")
+
+@app.get("/sort")
+def sort_patient(sort_by:str=Query(...,description="sort on the basis of height, weight, bmi and name."),order_by:str=Query('asc',description="Sort in ascending or descending order.")):
+
+    valid_fields=['height','weight','bmi','name','age']
+    order_fields=['asc','desc']
+    if sort_by not in valid_fields:
+        return HTTPException(status_code=400, detail=f'Invalid field, select from {valid_fields}')
+    if order_by not in order_fields:
+        return HTTPException(status_code=400, detail=f'Invalid order, select from {order_fields}')
+    data=load_data()
+    patients = list(data.values())
+
+    reverse = True if order_by.lower() == "desc" else False
+
+    # Sort based on given field
+    sorted_patients = sorted(
+        patients,
+        key=lambda x: x.get(sort_by, 0),
+        reverse=reverse
+    )
+
+    return sorted_patients
